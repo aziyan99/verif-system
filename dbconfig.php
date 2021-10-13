c8b81fbcb8464e4753805b37d8c5f990baa263b5
aziyan99 <rajaazian08@gmail.com>
2021-10-13 10:00:35 +0700

    update db credentials

--- dbconfig.php
+++ dbconfig.php
@@ -2,7 +2,7 @@
 $user  = 'uxmt5ayudvbi2bwl';
 $pass = 'pOvggjud5fzSCUU32hm8';
 try {
-    $conn = new PDO('mysql:host=btsj8anrhqktyxzqv0jx-mysql.services.clever-cloud.com;port:3306;dbname=btsj8anrhqktyxzqv0jx;', $user, $pass);
+    $conn = new PDO('mysql:host=btsj8anrhqktyxzqv0jx-mysql.services.clever-cloud.com;dbname=btsj8anrhqktyxzqv0jx;', $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     print "error : " . $e->getMessage() . "<br/>";
